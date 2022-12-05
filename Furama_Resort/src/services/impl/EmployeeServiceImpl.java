e7cbfbc712902933e492812dfcb495a431a3fec2
hau8477 <hau8477@gmail.com>
2022-12-05 12:07:13 +0700

    plan casestudy module2

--- Furama_Resort/src/services/impl/EmployeeServiceImpl.java
+++ Furama_Resort/src/services/impl/EmployeeServiceImpl.java
@@ -3,27 +3,58 @@ package services.impl;
 import models.persons.inheritance.Employee;
 import services.IEmployeeService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeServiceImpl implements IEmployeeService {
+    private final List<Employee> employees = new ArrayList<>();
+
     @Override
     public void editEmployee(Employee employee) {
-
+        for (Employee employee1 : employees) {
+            if (employee1.getEmployeeID() == employee.getEmployeeID()) {
+                employee1.setFullName(employee.getFullName());
+                employee1.setDayOfBirth(employee.getDayOfBirth());
+                employee1.setPhoneNumber(employee.getPhoneNumber());
+                employee1.setEmail(employee.getEmail());
+                employee1.setLevel(employee.getLevel());
+                employee1.setPosition(employee.getPosition());
+                employee1.setSalary(employee.getSalary());
+                break;
+            }
+        }
     }
 
     @Override
     public void deleteEmployee(String employeeID) {
+        Employee employeeDelete = null;
 
+        for (Employee employee : employees) {
+            if (employee.getEmployeeID() == employeeID) {
+                employeeDelete = employee;
+                break;
+            }
+        }
+        if (employeeDelete == null) {
+            return;
+        }
+
+        employees.remove(employeeDelete);
     }
 
     @Override
     public void addNew(Employee object) {
-
+        for (Employee employee : employees) {
+            if (employee.getEmployeeID() == object.getEmployeeID()) {
+                return;
+            }
+        }
+        employees.add(object);
     }
 
     @Override
     public List<Employee> displayList() {
-        return null;
+        return employees;
     }
 
     @Override
